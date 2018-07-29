3656bc05ccb0103884a1420564e57a1ae4de3a42
Matt Gordon <coltfanman@gmail.com>
2018-07-29 17:41:21 -0500

    stable version

--- bitmex_api.py
+++ bitmex_api.py
@@ -21,26 +21,22 @@ class Bitmex():
     def get_balance(self):
         return 50
 
+    def get_depth(self):
+        return self.ws.market_depth()
+
     def get_price(self):
-        return self.ws.get_ticker()
+        return self.ws.get_ticker()['last']
 
     def get_bid(self):
-        return self.get_orderbook('Buy')
+        return self.ws.get_ticker()['buy']
 
     def get_ask(self):
-        return self.get_orderbook('Sell')
+        return self.ws.get_ticker()['sell']
 
     def get_orderbook(self, direction):
         result = self.cl.OrderBook.OrderBook_getL2(symbol=self.symbol, depth=1).result()
         for r in result[0]:
             if r['side'] == direction:
-                return r['price']
-        return result
-
-    def get_ask(self):
-        result = self.cl.OrderBook.OrderBook_getL2(symbol=self.symbol, depth=1).result()
-        for r in result[0]:
-            if r['side'] == 'Sell':
                 return r['price']
         return result
 
@@ -54,9 +50,27 @@ class Bitmex():
     def get_recent(self):
         return self.ws.recent_trades()
 
-    def limit(self, qty, p):
-        result = self.cl.Order.Order_new(symbol=self.symbol, ordType='Limit', orderQty=qty, price=p).result()
-        return result
+    def get_funds(self):
+        return self.ws.funds()
+
+    def get_ticker(self):
+        return self.ws.get_ticker()
+
+    def get_position(self):
+        result = self.cl.Position.Position_get().result()
+        return result[0]
+
+    def limit(self, trade_no, link_id, qty, p):
+        for i in range(10):
+            try:
+                result = self.cl.Order.Order_new(symbol=self.symbol,
+                    clOrdID=trade_no, clOrdLinkID=link_id, ordType='Limit',
+                    orderQty=qty, price=p,
+                    execInst='ParticipateDoNotInitiate').result()
+                return result
+            except Exception as e:
+                print('received exception on limit, retrying...')
+                print(e)
 
     def take_profit(self, qty, trigger, p):
         result = self.cl.Order.Order_new(symbol=self.symbol, ordType='LimitIfTouched', orderQty=qty, stopPx=trigger, price=p, execInst='Close').result()
@@ -66,8 +80,30 @@ class Bitmex():
         result = self.cl.Order.Order_new(symbol=self.symbol, ordType='Stop', orderQty=qty, stopPx=p, execInst='Close').result()
         return result
 
-    def set_stops(self, link_id, qty, stop, tp):
-        res1 = self.cl.Order.Order_new(symbol=self.symbol, clOrdLinkID=link_id, contingencyType='OneCancelsTheOther', ordType='Limit', orderQty=qty, price=tp, execInst='Close').result()
-        res2 = self.cl.Order.Order_new(symbol=self.symbol, clOrdLinkID=link_id, contingencyType='OneCancelsTheOther', ordType='Stop', orderQty=qty, stopPx=stop, execInst='Close').result()
-        print(res1)
-        print(res2)
+    def set_stops(self, trade_no, link_id, qty, stop, tp, trigger):
+        for attempt in range(10):
+            try:
+                res1 = self.cl.Order.Order_new(symbol=self.symbol, clOrdID='tp' + trade_no, clOrdLinkID=link_id, contingencyType='OneUpdatesTheOtherAbsolute', ordType='LimitIfTouched', orderQty=qty, price=tp, stopPx=trigger, execInst='Close, LastPrice').result()
+                res2 = self.cl.Order.Order_new(symbol=self.symbol, clOrdID='stop' + trade_no, clOrdLinkID=link_id, contingencyType='OneUpdatesTheOtherAbsolute', ordType='Stop', orderQty=qty, stopPx=stop, execInst='Close, LastPrice').result()
+            except:
+                print('received exception, retrying...')
+            else:
+                break
+        else:
+            print('failed after 10 attempts, cancelling orders...')
+            self.cancel_orders()
+
+    def update_price(self, trade_no, price, stop, tp, trigger):
+        for attempt in range(10):
+            try:
+                result = self.cl.Order.Order_amend(origClOrdID=trade_no, price=price).result()
+                res1 = self.cl.Order.Order_amend(origClOrdID='tp' + trade_no, price=tp, stopPx=trigger).result()
+                res2 = self.cl.Order.Order_amend(origClOrdID='stop' + trade_no, stopPx=stop).result()
+            except Exception as e:
+                print('error when updating price, retrying...')
+            else:
+                break
+
+    def cancel_orders(self):
+        result = self.cl.Order.Order_cancelAll(symbol='XBTUSD').result()
+        return result
--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+import signal
+import sys
+
 import bitmex_api
 import mail
 import datetime
@@ -6,6 +9,7 @@ class Bot():
     def __init__(self):
         self.trades = {}
         self.last_message = 0
+        self.trade_no = 19
         self.last_update = datetime.datetime.now()
 
         self.mail = mail.Mail()
@@ -14,27 +18,130 @@ class Bot():
 
         self.percent = .02
 
-    def calc_size(self, price, spread):
-        balance = self.bitmex.get_balance()
+    def calc_size(self, price, _range):
+        # balance = self.bitmex.get_balance()
+        balance = 30
         n1 = price / 100.0
-        n2 = (balance * self.percent) / (spread / price * 100.0))
+        n2 = (balance * self.percent) / (_range / price * 100.0)
         return n1 * n2
 
+    def is_open(self):
+        orders = self.bitmex.get_orders()
+        for order in orders:
+            if order['ordType'] == 'Limit':
+                return True
+        return False
+
+    def enter_trade(self, message):
+        _range = message['range']
+        size = self.calc_size(self.bitmex.get_price(), _range)
+        price = 0
+
+        self.trade_no += 1
+        self.trades[self.trade_no] = {}
+
+        if message['direction'] == 'long':
+            print('last: {}'.format(self.bitmex.get_price() - .5))
+            print('bid: {}'.format(self.bitmex.get_bid()))
+            price = max(self.bitmex.get_bid(), self.bitmex.get_price() - .5)
+            print('entering trade at {}'.format(price))
+
+            self.bitmex.limit(str(self.trade_no), 'link' + str(self.trade_no),
+                size, price)
+            self.bitmex.set_stops(str(self.trade_no),
+                'link' + str(self.trade_no), -size, price - _range, price + _range,
+                price + _range - 1.0)
+        elif message['direction'] == 'short':
+            print('last: {}'.format(self.bitmex.get_price() + .5))
+            print('ask: {}'.format(self.bitmex.get_ask()))
+            price = min(self.bitmex.get_ask(), self.bitmex.get_price() + .5)
+            print('entering trade at {}'.format(price))
+
+            self.bitmex.limit(str(self.trade_no), 'link' + str(self.last_message), -size, price)
+            self.bitmex.set_stops(str(self.trade_no), 'link' + str(self.last_message),
+                size, price + _range, price - _range, price - _range + 1.0)
+        else:
+            print('something wrong: direction not valid: {}'.format(direction))
+            return
+        
+        self.trades[self.trade_no] = {
+            'direction': message['direction'],
+            'price': price,
+            'range': _range,
+            'age': 0,
+        }
+
     def run(self):
-        price = self.bitmex.get_ask()
-        self.bitmex.set_stops('id', -1, price - 50, price)
+        # price = self.bitmex.get_ask()
+        # self.bitmex.set_stops('id', -1, price - 50, price)
+        # mymsg = {"num": 5, "direction": "long", "range": 40}
+        # self.enter_trade(mymsg)
 
         while True:
             t = datetime.datetime.now()
-            if t.second == 5 and (t - self.last_update).total_seconds() > 10:
+            if t.second == 5 and (t - self.last_update).total_seconds() > 5:
                 self.last_update = t
-                print('Price: ' + str(self.bitmex.get_price()))
+
+                # first clear any trade still open
+                if self.is_open():
+                    age = self.trades[self.trade_no]['age']
+                    if age <= 0:
+                        print('adding to age of order...')
+                    elif age > 0:
+                        print('order too old, cancelling...')
+                        self.bitmex.cancel_orders()
+                    self.trades[self.trade_no]['age'] += 1
+
+                # checking for new message. If there is a new message, enter
+                # the trade
                 message = self.mail.check_for_mail()
                 if message:
-                    print('Message Received: ' + str(message))
-                else:
-                    print('No message')
+                    positions = self.bitmex.get_position()
+                    for i in positions:
+                        if i['symbol'] == 'XBTUSD':
+                            positions = i['currentQty']
+
+                    if self.last_message != 0 and positions == 0:
+                        print('Message Received: ' + str(message))
+                        self.enter_trade(message)
+                    else:
+                        print('ignoring first message')
+                    self.last_message += 1
+            elif t.second == 15 and (t - self.last_update).total_seconds() > 5:
+                self.last_update = t
+                if self.is_open():
+                    trade_price = self.trades[self.trade_no]['price']
+                    curr_price = self.bitmex.get_price()
+                    dirr = self.trades[self.trade_no]['direction']
+                    _range = self.trades[self.trade_no]['range']
+                    if dirr == 'long':
+                       bid = self.bitmex.get_bid()
+                       new_price = max(bid, curr_price - .5)
+                       if (new_price - trade_price > 1 and new_price - trade_price < 20):
+                           print('detected price difference, updating...')
+                           self.trades[self.trade_no]['price'] = new_price
+                           self.bitmex.update_price(str(self.trade_no), new_price, new_price - _range, new_price + _range, new_price + _range - 1)
+                    elif dirr == 'short':
+                       ask = self.bitmex.get_ask()
+                       new_price = min(ask, curr_price + .5)
+                       if (trade_price - new_price > 1 and trade_price - new_price < 20):
+                           print('detected price difference, updating...')
+                           self.trades[self.trade_no]['price'] = new_price
+                           self.bitmex.update_price(str(self.trade_no), new_price, new_price + _range, new_price - _range, new_price - _range + 1)
+                
+
+# def signal_handler(sig, frame):
+#     print('received kill signal')
+#     sys.exit(0)
+# 
+# signal.signal(signal.SIGINT, signal_handler)
 
 
-bot = Bot()
-bot.run()
+while True:
+    try:
+        bot = Bot()
+        bot.run()
+    except Exception as e:
+        print('ran into exception {}\n, re-running...'.format(str(e)))
+    else:
+        break
